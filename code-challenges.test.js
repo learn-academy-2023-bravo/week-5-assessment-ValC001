f4c1404f15eee98e75ec4ae74ed4ada4b2598d0b
Valeria <valeriacasillas123@yahoo.com>
2023-04-17 09:38:14 -0700

    wek5

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -21,19 +21,36 @@ const secretCodeWord3 = "Eccentric"
 //Pseudo code
 // Create a describe statement 
 describe ("messageCoder", () => {
-    it("takes in a function and returns a string with a coded message. The coded message converts "a" to 4, "e" to 3, "i" to 1, and "o" to 0.",  () =>{
-        const secrectCodeWord1 = "Lackadaisical"
-        const secretCodeWord2 = "Gobbledygook"
-        const secretCodeWord3 = "Eccentric"
+//     it("takes in a function and returns a string with a coded message. The coded message converts "a" to 4, "e" to 3, "i" to 1, and "o" to 0.",  () =>{
+//         const secrectCodeWord1 = "Lackadaisical"
+//         const secretCodeWord2 = "Gobbledygook"
+//         const secretCodeWord3 = "Eccentric"
          
-        expect(messageCoder(secrectCodeWord1)).toEqual("L4ck4d41s1c4l")
-        expect(messageCoder(secrectCodeWord2)).toEqual("Gobbledygook")
-        expect(messageCoder(secrectCodeWord3)).toEqual("3cc3ntr1c")
-    })
-})
+//         expect(messageCoder(secrectCodeWord1)).toEqual("L4ck4d41s1c4l")
+//         expect(messageCoder(secrectCodeWord2)).toEqual("Gobbledygook")
+//         expect(messageCoder(secrectCodeWord3)).toEqual("3cc3ntr1c")
+//     })
+// })
 
+// FAIL  ./code-challenges.test.js
+// ● Test suite failed to run
 
+//   SyntaxError: /Users/learnacademy/Desktop/week-5-assessment-ValC001/code-challenges.test.js: Unexpected token, expected "," (24:99)
 
+    22 | // Create a describe statement 
+//     23 | describe ("messageCoder", () => {
+//   > 24 |     it("takes in a function and returns a string with a coded message. The coded message converts "a" to 4, "e" to 3, "i" to 1, and "o" to 0.",  () =>{
+//        |                                                                                                    ^
+//     25 |         const secrectCodeWord1 = "Lackadaisical"
+//     26 |         const secretCodeWord2 = "Gobbledygook"
+//     27 |         const secretCodeWord3 = "Eccentric"
+
+//     Test Suites: 1 failed, 1 total
+//     Tests:       0 total
+//     Snapshots:   0 total
+//     Time:        0.875 s
+//     Ran all test suites matching /code-challenges.test.js/i.
+//     error Command failed with exit code 1.
 
 
 // b) Create the function that makes the test pass.
@@ -65,11 +82,11 @@ describe ("messageCoder", () => {
 // 2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
 
-const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+// const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
 
-const letterA = "a"
+// const letterA = "a"
 // Expected output: ["Mango", "Apricot", "Peach"]
-const letterE = "e"
+// const letterE = "e"
 // Expected output: ["Cherry", "Blueberry", "Peach"]
 
 // a) Create a test with expects statement using the variable provided.
@@ -82,13 +99,20 @@ describe("vowelFavorerer", ()=> {
 
         expect(vowelFavorerer(letterA)).toEqual("Mango", "Apricot", "Peach")
         expect(vowelFavorerer(letterE)).toEqual("Cherry", "Blueberry", "Peach")
+    
     })
-})
+ })
 // b) Create the function that makes the test pass.
 
 //Psuedo code 
-//
+//For this code I can use a similar method to how I did ruby challenge 1. I'll be using sort & include so I can filter out the words based on what I want it to include.
+ const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+  function vowelFavorerer(fruitArray){
+    const vowelFavorerer = fruitArray.filter(fruitArray => fruitArray.includes('e')).sort();
+    return vowelFavorerer
+  }
 
+console.log(vowelFavorerer(fruitArray));
 
 
 
